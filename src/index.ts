b67e104604de303d40a945e1694e2c11a24417d8
dev-parvej <parvej.code@gmail.com>
2021-11-07 01:13:51 +0600

    Added synch chain

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import bodyParser from "body-parser";
 import router from "./router";
+import request from "request";
 
 const app = express();
+let PORT = 5000;
+const ROOT_NODE_URL = `http://localhost:${PORT}`
 
 app.use(bodyParser.json())
 app.use(bodyParser.raw())
@@ -12,7 +15,13 @@ app.get('/', (_, res) => {
     res.status(200).send('HIs')
 })
 
-let PORT = 5000;
+const syncChain = () => {
+    request(`${ROOT_NODE_URL}/api/v1/blocks`, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+
+        }
+    })
+}
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
     PORT += Math.ceil(Math.random() * 1000)
